7a38425f822dc6bafc2c43e8ea502f7071c1ecf9
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-05-06 03:49:48 +0000

    Add detach_input to ClassificationHead

--- segmentation_models_pytorch/base/heads.py
+++ segmentation_models_pytorch/base/heads.py
@@ -13,7 +13,7 @@ class SegmentationHead(nn.Sequential):
 
 class ClassificationHead(nn.Sequential):
 
-    def __init__(self, in_channels, classes, pooling="avg", dropout=0.2, activation=None):
+    def __init__(self, in_channels, classes, pooling="avg", dropout=0.2, activation=None, detach_input=False):
         if pooling not in ("max", "avg"):
             raise ValueError("Pooling should be one of ('max', 'avg'), got {}.".format(pooling))
         pool = nn.AdaptiveAvgPool2d(1) if pooling == 'avg' else nn.AdaptiveMaxPool2d(1)
@@ -22,3 +22,9 @@ class ClassificationHead(nn.Sequential):
         linear = nn.Linear(in_channels, classes, bias=True)
         activation = Activation(activation)
         super().__init__(pool, flatten, dropout, linear, activation)
+        self._detach_input = detach_input
+
+    def forward(self, input):
+        if self._detach_input:
+            input = input.detach()
+        return super().forward(input)
